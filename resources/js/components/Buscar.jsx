ea61f033ba88a83bd072dccdeda70480f54d0fb4
Damián de Vera <ddevhor@gmail.com>
2023-02-24 20:23:10 +0100

    Filtrado de rutas por origen

--- resources/js/components/Buscar.jsx
+++ resources/js/components/Buscar.jsx
@@ -1,11 +1,37 @@
-import { usePage } from '@inertiajs/react'
+import React from 'react';
+
+import { useState } from 'react'
+import { router, usePage } from '@inertiajs/react'
 
 export default function Buscar(props) {
+    const { errors } = usePage().props
+
+    const [values, setForm] = useState({
+        origin: ""
+    })
+
+    function handleChange(e) {
+        const key = e.target.id;
+        const value = e.target.value
+        setForm(values => ({
+            ...values,
+            [key]: value,
+        }))
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        router.get('/buscar', values)
+    }
     let rutas = props.travel;
-    console.log(props)
     return (
 
         <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="origin">Origen</label>
+                <input id="origin" value={values.origin} onChange={handleChange} />
+                <button type="submit">Buscar</button>
+            </form>
             {rutas.map((ruta, index) => {
                 return (
                     <div key={index}>
