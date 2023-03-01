c201ad6744d9463573e3f0803761fc31d1680c3e
Adrian Glez <97676487+adrianglez16@users.noreply.github.com>
2023-03-01 12:25:16 +0000

    Update Buscar.jsx

--- resources/js/components/Buscar.jsx
+++ resources/js/components/Buscar.jsx
@@ -28,40 +28,37 @@ export default function Buscar(props) {
     let rutas = props.travel;
     return (
 
-        <div className='container'>
-            <div className="row">
-                <div class="col-12 my-2">
-                    <h1>Busquedas con filtro:</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="date">Fecha</label>
-                        <input id="date" type="date" value={values.date} onChange={handleChange} />
-                        <button type="submit">Buscar</button>
-                    </form>
-                </div>
-            </div>
-            <div className="row">
-                <div class="col-3 my-3">
+        <div>
+            <h1>Busquedas con filtro:</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="origin">Origen</label>
+                <input id="origin" value={values.origin} onChange={handleChange} />
+                <label htmlFor="destination">Destino</label>
+                <input id="destination" value={values.destination} onChange={handleChange} />
+                <button type="submit">Buscar</button>
+            </form>
+            {rutas.map((ruta, index) => {
+                return (
+                    <div key={index}>
+                        <h2>Origen: {ruta.origin}</h2>
+                        <h2>Destino: {ruta.destination}</h2>
+                        <h2>Fecha: {ruta.date}</h2>
+                        <h2>Hora: {ruta.hour}</h2>
+                        <h2>Asientos: {ruta.seats}</h2>
+                        <h2>Conductor: {ruta.user.name}</h2>
+                        {user == null &&
 
-                    {rutas.map((ruta, index) => {
-                        return (
-                            <div key={index}>
-                                <div class="card " >
-                                    <div class="card-body">
-                                        <h3 class="card-title" >Origen: {ruta.origin}</h3>
-                                        <p class="card-text">Destino: {ruta.destination}</p>
-                                        <p class="card-text">Fecha: {ruta.date}</p>
-                                        <p class="card-text">Hora: {ruta.hour}</p>
-                                        <p class="card-text">Asientos: {ruta.seats}</p>
-                                        <p class="card-text">Conductor: {ruta.user.name}</p>
-                                        <Reservar user={props.user} travel={ruta} />
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
+                            <Link href="/loginReact" class='btn btn-primary'>Reservar</Link>
+                        }
+                        {user != null &&
 
-        </div >
+                            <Reservar user={props.user} travel={ruta} />
+                        }
+                        <hr />
+                    </div>
+                );
+            })}
+
+        </div>
     );
 }
