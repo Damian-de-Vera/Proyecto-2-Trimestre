4fd0e29ecfcbc2e7c0fc4dc6cc517947410b75ce
Adrian Glez <97676487+adrianglez16@users.noreply.github.com>
2023-03-01 15:05:07 +0000

    Update PerfilPage.jsx

--- resources/js/Pages/PerfilPage.jsx
+++ resources/js/Pages/PerfilPage.jsx
@@ -2,6 +2,8 @@ import Nav from '@/Pages/Layouts/Components/Nav'
 import Footer from '@/Pages/Layouts/Components/Footer'
 import Perfil from '@/Pages/Layouts/Perfil'
 
+import Avatar from '@/Pages/Layouts/Avatar'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function PerfilPage(props) {
@@ -9,6 +11,7 @@ export default function PerfilPage(props) {
     return (
         <div>
             <Nav user={props.user} />
+            <Avatar user={props.user} />
             <Perfil user={props.user} />
             <Footer />
         </div>
