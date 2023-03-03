db6bd1fc475e3b33423f47fc000761f7057da03b
adrianglez16 <adriannglez6@gmail.com>
2023-03-03 23:53:36 +0000

    admin dashboard completado menos el update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,20 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Queries\UserQuery;
-use App\Queries\UsersQuery;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Inertia\Inertia;
-use Illuminate\Support\Facades\Validator;
 
+/**
+ * Class UserController
+ * @package App\Http\Controllers
+ */
 class UserController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
     /**
      * Display a listing of the resource.
      *
@@ -25,13 +21,12 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
-        // return view('user.index')->with('users', $users);
+        $users = User::paginate();
 
-        return view('users.index', compact('users'))->with('users', $users);
+        return view('user.index', compact('users'))
+            ->with('i', (request()->input('page', 1) - 1) * $users->perPage());
     }
 
-  
     /**
      * Show the form for creating a new resource.
      *
@@ -39,69 +34,69 @@ class UserController extends Controller
      */
     public function create()
     {
-        $users = new User();
-        return view('users.create', compact('users'));
+        $user = new User();
+        return view('user.create', compact('user'));
     }
 
-   
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-  
+        request()->validate(User::$rules);
+
+        $user = User::create($request->all());
+
+        return redirect()->route('users.index')
+            ->with('success', 'User created successfully.');
     }
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'max:255'],
-            'password' => ['required'],
-        ]);
-    }
+
+    // protected function validator(array $data)
+    // {
+    //     return Validator::make($data, [
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'email' => ['required', 'string', 'max:255'],
+    //         'password' => ['required'],
+    //     ]);
+    // }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\User  $User
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function show(User $User)
+    public function show($id)
     {
+        $user = User::find($id);
+
+        return view('user.show', compact('user'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\User  $User
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(User $User)
+    public function edit($id)
     {
-        //
+        $user = User::find($id);
+
+        return view('user.edit', compact('user'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $User
+     * @param  \Illuminate\Http\Request $request
+     * @param  User $user
      * @return \Illuminate\Http\Response
      */
-
-     public function AllUser()
-     {
-         $all = DB::table('users')->get()->paginate(25);
-         return view('users.all-user', compact('all'));
-     }
- 
-
-
-
-    public function update(Request $request, User $User)
+    public function update(Request $request, User $user)
     {
         Session::flash('message', 'Perfil actualizado');
         $currentUser = Auth::user();
@@ -112,16 +107,6 @@ class UserController extends Controller
         // return Inertia::render('PerfilPage', ['user' => $result]);
     }
 
-    public function update2(Request $request, User $user)
-    {
-        request()->validate(User::$rules);
-
-        $user->update($request->all());
-
-        return redirect()->route('users.index')
-            ->with('success', 'user updated successfully');
-    }
-
     public function avatar(Request $request)
     {
         $currentUser = Auth::user();
@@ -129,19 +114,16 @@ class UserController extends Controller
         $result = $query->updateAvatar($request, $currentUser);
     }
 
-
-
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $User
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
      */
     public function destroy($id)
     {
         $user = User::find($id)->delete();
 
         return redirect()->route('users.index')
-            ->with('success', 'Costumer deleted successfully');
+            ->with('success', 'User deleted successfully');
     }
 }
