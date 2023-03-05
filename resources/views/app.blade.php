b1e76e01feec17929b57165e84be48cdde9ca63b
adrianglez16 <adriannglez6@gmail.com>
2023-03-05 22:55:23 +0000

    background

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,28 +12,37 @@
     <!-- Bootstrap icons-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet"
-        type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
     <!-- My styles -->
     <style>
-    .card {
-        margin-top: 1.5rem;
-    }
+        .card {
+            margin-top: 1.5rem;
+        }
 
-    .showcase3 {
-        background-image: url('assets/img/bg-showcase-3.jpg')
-    }
+        .showcase3 {
+            background-image: url('assets/img/bg-showcase-3.jpg')
+        }
 
-    .showcase2 {
-        background-image: url('assets/img/bg-showcase-2.jpg')
-    }
+        .showcase2 {
+            background-image: url('assets/img/bg-showcase-2.jpg')
+        }
 
-    .showcase1 {
-        background-image: url('assets/img/bg-showcase-1.jpg')
-    }
+        .showcase1 {
+            background-image: url('assets/img/bg-showcase-1.jpg')
+        }
 
-    /*   footer {
+        body {
+            /* background-image: url('assets/movifpsostenible_v2.ico');
+        background-repeat: no-repeat;
+        background-position: center;
+        background-attachment: fixed;
+        background-size:contain;     */
+            background: rgb(247, 251, 247);
+            background: linear-gradient(0deg, rgba(247, 251, 247, 1) 34%, rgba(199, 235, 205, 1) 65%, rgba(192, 223, 198, 1) 89%);
+        }
+
+        /*   footer {
         position: fixed;
         height: 100px;
         bottom: 0;
@@ -42,8 +51,8 @@
     </style>
 
     @viteReactRefresh
-    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"]) 
-    
+    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+
     @inertiaHead
 </head>
 
