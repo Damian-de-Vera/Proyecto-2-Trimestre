076738d2dd663725f29e377a5ed18d65e30ede0a
Damián de Vera <ddevhor@gmail.com>
2023-03-05 20:55:19 +0100

    Footer arreglado

--- resources/js/Pages/Layouts/Components/Perfil.jsx
+++ resources/js/Pages/Layouts/Components/Perfil.jsx
@@ -34,7 +34,7 @@ function Perfil(props) {
     }
     return (
 
-        <div class="container">
+        <div class="container min-vh-100">
             {flash.message && (
                 <div class="alert alert-success d-flex align-items-center py-3" role="alert">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
--- resources/js/Pages/Layouts/Components/Publicar.jsx
+++ resources/js/Pages/Layouts/Components/Publicar.jsx
@@ -35,7 +35,7 @@ function Publicar(props) {
     }
     return (
 
-        <div class="container">
+        <div class="container min-vh-100">
             {flash.message && (
                 <div class="alert alert-success d-flex align-items-center py-3" role="alert">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
